3c8d4ef3875623203dacec89bedc4c1fbfd1f489
Charles Gifford <charlesgifford@charless-mbp.bc.edu>
2026-04-01 15:17:41 -0400

    update CORS for production

--- route-generator-api/app/main.py
+++ route-generator-api/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Route Generator API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite dev server
+    allow_origins=["https://route-generator-eight.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
